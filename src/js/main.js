46020475bec69fc9be48be9d1515da3fe261910e
= <=>
2018-05-04 03:05:47 -0300

    finalizando portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,85 +1,79 @@
-$(function(){
-   $("html").animate({scrollTop:0}, 600);
-   AOS.init({
-    duration: 1200,
-  })
-  
+$(document).ready(function () {
 
-  
-    
-   /*  $(".link").click(function () {
-        var nav = $('nav')
-            , nav_height = nav.outerHeight();
-        $(".link").parent().removeClass('active');
-        $(this).parent().addClass('active');
-        var active_section = $(this).attr('href'); //get active section id
-        $('html, body').animate({
-            //and scroll to the section
-            scrollTop: $(active_section).offset().top - nav_height
-            }, 1000);
-          }); */
-    
-    
-    
-    
+    setTimeout(function () {
+        $("#preloader").remove();
+    }, 1000);
+
+
+    $('img[alt="000webhost logo"]').hide();
+
+    $("html").animate({
+        scrollTop: 0
+    }, 600);
+    AOS.init({
+        duration: 1200,
+    })
+
+
+
     funcoes.eventos.trocarCorMenu();
     funcoes.eventos.timer();
     funcoes.efeitos.preloader();
-    
+
     funcoes.efeitos.efeitoLazy();
     funcoes.efeitos.efeitoLightbox();
     funcoes.efeitos.efeitoHr();
     funcoes.efeitos.efeitoTyped();
     funcoes.eventos.voltarAoTopo();
-    
+
     // Ajustando formato do cursor
     $(".typed-cursor").css("font-size", "100px");
     $(".typed-cursor").css("margin-left", "2px");
-    
-    
-      funcoes.eventos.slidePage(".link-seta", "#sobre");
-  /* funcoes.eventos.slidePage("#link-portfolio", "#portfolio");
-    funcoes.eventos.slidePage("#link-home", "#destaque");
-    funcoes.eventos.slidePage("#link-sobre", "#sobre"); */
-    
-    
+
+
+    funcoes.eventos.slidePage(".link-seta", "#sobre");
+    /* funcoes.eventos.slidePage("#link-portfolio", "#portfolio");
+      funcoes.eventos.slidePage("#link-home", "#destaque");
+      funcoes.eventos.slidePage("#link-sobre", "#sobre"); */
+
+
 });
 
 var funcoes = {
     efeitos: {
         // Fade in ao interagir com o scroll
-        efeitoLazy: function(){
+        efeitoLazy: function () {
             $('.lazy').Lazy({
                 scrollDirection: 'vertical',
                 effect: 'fadeIn',
                 visibleOnly: true,
-                onError: function(element) {
+                onError: function (element) {
                     console.log('error loading ' + element.data('src'));
                 }
             });
         },
-        
+
         // Adiciona efeito no menu ao passar o mouse
-        efeitoHr: function(){
+        efeitoHr: function () {
             var toggle = false,
-            hrs = $('.trans-grow');
+                hrs = $('.trans-grow');
             //$('.navbar').hover(function(){
-            $.map(hrs, function(val, i){
-                if(toggle === false){
+            $.map(hrs, function (val, i) {
+                if (toggle === false) {
                     $(val)
-                    .addClass('grow')
-                    .addClass('line-color-change'); 
-                }else{
+                        .addClass('grow')
+                        .addClass('line-color-change');
+                } else {
                     $(val).removeClass('grow')
-                    .removeClass('line-color-change');
+                        .removeClass('line-color-change');
                 }
-            }); 
+            });
             toggle = !toggle;
             //});
         },
-        
+
         // Efeito de digitação automática
-        efeitoTyped: function(){
+        efeitoTyped: function () {
             var typed = new Typed('.titulo-principal', {
                 stringsElement: '#typed-conteudo',
                 typeSpeed: 55,
@@ -89,139 +83,136 @@ var funcoes = {
                 smartBackspace: true,
                 fadeOut: true
             });
-            
+
             // Ajuste no tempo até o título principal ser exibido
             $(".titulo-principal").fadeIn(2000);
-            
+
             // Ajuste no tempo até que a mensagem desapareça 
-            setTimeout(function() {
-                $("p.animacao1").addClass("fadeOutLeft"); 
-            },7000);
+            setTimeout(function () {
+                $("p.animacao1").addClass("fadeOutLeft");
+            }, 7000);
         },
-        
-        efeitoLightbox: function(){
+
+        efeitoLightbox: function () {
             lightbox.option({
                 'resizeDuration': 200,
                 'wrapAround': true,
                 'alwaysShowNavOnTouchDevices': true,
                 'maxWidth': 1500,
                 'positionFromTop': 100
-                
+
             });
         },
-        
-        preloader: function(){
-            $(window).on("load",function (){
+
+        preloader: function () {
+            $(window).on("load", function () {
                 $("#preloader").fadeOut(500);
             });
         },
     },
     eventos: {
-        trocarCorMenu: function(){
-            
-            var sections = $('section, header')
-            , nav = $('nav')
-            , nav_height = nav.outerHeight();
-            
+        trocarCorMenu: function () {
+
+            var sections = $('section, header'),
+                nav = $('nav'),
+                nav_height = nav.outerHeight();
+
             $(window).on('scroll', function () {
                 var cur_pos = $(this).scrollTop();
-                
-                sections.each(function() {
-                    var top = $(this).offset().top - nav_height,
-                    bottom = top + $(this).outerHeight();
-                    
+
+                sections.each(function () {
+                    var top = $(this).offset().top - 80,
+                        bottom = top + $(this).outerHeight();
+
                     if (cur_pos >= top && cur_pos < bottom) {
                         nav.find('a').parent().removeClass('active');
                         sections.removeClass('active');
-                        
-                        
-                        nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+
+
+                        nav.find('a[href="#' + $(this).attr('id') + '"]').parent().addClass('active');
                     }
                 });
             });
-            
+
             nav.find('a').on('click', function () {
-                var $el = $(this)
-                , id = $el.attr('href');
-                
+                var $el = $(this),
+                    id = $el.attr('href');
+
                 $('html, body').animate({
                     scrollTop: $(id).offset().top - nav_height
                 }, 500);
-                
+
                 return false;
             });
-            
+
         },
-        
-        timer: function(){
-            
+
+        timer: function () {
+
             var today = new Date();
-            
-            
+
+
             var BigDay = new Date("October 3, 2016");
             var msPerDay = 24 * 60 * 60 * 1000;
-            var timeLeft = (today.getTime() - BigDay.getTime()  );
+            var timeLeft = (today.getTime() - BigDay.getTime());
             var e_daysLeft = timeLeft / msPerDay;
             var daysLeft = Math.floor(e_daysLeft);
             var yearsLeft = 0;
             var mounthLeft = 0;
-            
-            
+
+
             var sAno = "";
             var sDia = "";
             if (daysLeft > 365) {
                 yearsLeft = Math.floor(daysLeft / 365);
                 daysLeft = daysLeft % 365;
             }
-            
-            
-            if(daysLeft > 31){
+
+
+            if (daysLeft > 31) {
                 mounthLeft = Math.floor(daysLeft / 30);
                 daysLeft = (daysLeft % 30);
             }
-            
-            if(yearsLeft <= "1"){
+
+            if (yearsLeft <= "1") {
                 sAno = " ano";
-            }
-            else{
+            } else {
                 sAno = " anos"
             }
-            
-            
-            if(mounthLeft <= "1"){
+
+
+            if (mounthLeft <= "1") {
                 sMes = " mes";
-            }
-            else{
+            } else {
                 sMes = " meses"
             }
-            
-            if(daysLeft <= "1"){
+
+            if (daysLeft <= "1") {
                 sDia = " dia";
-            }
-            else{
+            } else {
                 sDia = " dias"
             }
-            
-            
-            $("#timer").html(yearsLeft +  sAno + ", " + mounthLeft + sMes + " e " + daysLeft + sDia);
-            
+
+
+            $("#timer").html(yearsLeft + sAno + ", " + mounthLeft + sMes + " e " + daysLeft + sDia);
+
         },
-        
+
         // Botão é adicionado ao interagir com o scroll
-        voltarAoTopo: function(){
+        voltarAoTopo: function () {
             if ($('#voltar-ao-topo').length) {
                 var scrollTrigger = 100, // px
-                backToTop = function () {
-                    var scrollTop = $(window).scrollTop();
-                    if (scrollTop > scrollTrigger) {
-                        $('#voltar-ao-topo').addClass('show');
-                    } else {
-                        $('#voltar-ao-topo').removeClass('show');
-                    }
-                };
+                    backToTop = function () {
+                        var scrollTop = $(window).scrollTop();
+                        if (scrollTop > scrollTrigger) {
+                            $('#voltar-ao-topo').addClass('show');
+                        } else {
+                            $('#voltar-ao-topo').removeClass('show');
+                        }
+                    };
                 backToTop();
                 $(window).on('scroll', function () {
-                    backToTop();              
+                    backToTop();
                 });
                 $('#voltar-ao-topo').on('click', function (e) {
                     e.preventDefault();
@@ -231,18 +222,18 @@ var funcoes = {
                 });
             }
         },
-        slidePage: function(element, target){
-            $(element).on('click', function(e) {
-           
+        slidePage: function (element, target) {
+            $(element).on('click', function (e) {
+
                 e.preventDefault();
                 var id = $(this).attr('href'),
-                targetOffset = $(target).offset().top -44 ;
-                
-                $('html, body').animate({ 
+                    targetOffset = $(target).offset().top - 44;
+
+                $('html, body').animate({
                     scrollTop: targetOffset
                 }, 600);
             });
-            
+
         }
     }
 }
